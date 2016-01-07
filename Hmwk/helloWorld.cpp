123d84833fc4f681243e1c8c80f027f7cfab72b1
johnyou1 <johnyou901029@gmail.com>
2016-01-07 14:55:03 -0800

    Signed off 2:55 PM

---
+++ Hmwk/helloWorld.cpp
@@ -0,0 +1,22 @@
+/*
+Author: John You
+Date: 2016/01/06 
+Purpose: Create a Hello World Program
+*/
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// User Libraries
+
+// Function Prototypes
+
+// Global Constants
+
+// Exeuction begins here
+int main( int argc, char **argv ) {
+	cout << "Hello, World!" << endl;
+	
+	return 0;
+}
